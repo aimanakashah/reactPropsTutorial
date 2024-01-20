f7496ff083cad1895fb77fd16469a67f85bef324
aimanakashah <aimanakashah@gmail.com>
2024-01-20 18:31:45 +0800

    Mapping Components Practice

---
+++ 6.0 Mapping Components Practice/src/emojipedia.js
@@ -0,0 +1,33 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+
+  {
+    id: 4,
+    emoji: "😍",
+    name: "Smiling Face With Heart-Eyes",
+    meaning:
+      "A smile of love is shown by the 😍 emoji, which beams with happiness with its heart-shaped eyes. It conveys intense love and affection for a person, place or even an object.",
+  },
+];
+
+export default emojipedia;
