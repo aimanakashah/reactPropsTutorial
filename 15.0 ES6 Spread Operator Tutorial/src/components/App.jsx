525c1c6303d9d6c694a21a3d2ce4b46fe85058b0
aimanakashah <aimanakashah@gmail.com>
2024-01-28 22:40:49 +0800

    Commit for ES6 Spread Operator Tutorial.

---
+++ 15.0 ES6 Spread Operator Tutorial/src/components/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value, //this will override the value of the [name] inserted in the preValue
+      };
+
+      // ({ ...prevValue, [name]: value }); //can be used instead of return above since it is a singular object
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
