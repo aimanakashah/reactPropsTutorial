42dc72e896846809fc7a85e0f59993f86ef930cb
aimanakashah <aimanakashah@gmail.com>
2024-01-30 23:24:16 +0800

    Comment added to explain the feature.

--- 15.1 ES6 Spread Operator Practice/src/components/App.jsx
+++ 15.1 ES6 Spread Operator Practice/src/components/App.jsx
@@ -10,18 +10,17 @@ function App() {
   }
 
   function addLists() {
-    console.log(lists);
-
     setList((prevLists) => [
       ...prevLists,
       { key: lists.length + 1, note: inputValue },
-    ]);
-    setInputValue("");
+    ]); //inputValue was passed from the handlChange() when the event.target.value was inserted into setInputValue()
+    setInputValue(""); //act as an operator to pass the note inserted (inputValue) before storing the note into array 'lists'
+    console.log(lists);
   }
 
   function handleChange(event) {
     const { name, value } = event.target;
-    setInputValue(value);
+    setInputValue(value); //setInputValue(inputValue)
 
     // setList((prevValue) => {
     //   return [...prevValue, { note: value }];
@@ -32,7 +31,7 @@ function App() {
     // if (event.key === "Enter") {
     //   addLists();
     // }
-    return event.key === "Enter" ? addLists() : null;
+    return event.key === "Enter" ? addLists() : null; //extra feature added so that the note was added when the user press 'Enter'.
   }
 
   return (
@@ -44,6 +43,7 @@ function App() {
         <input
           onKeyDown={handleKey}
           value={inputValue}
+          //value was set so that we can clear the input placeholder after the note was added
           onChange={handleChange}
           type="text"
         />
@@ -51,6 +51,7 @@ function App() {
           <span>Add</span>
         </button>
       </div>
+      {/* using mapping components technique to display note added like a forEach and for loop */}
       {lists.map(createList)}
     </div>
   );
