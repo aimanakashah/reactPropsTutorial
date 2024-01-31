00d06b59dc0bcc15e3793d547df528ca6f76eb3e
aimanakashah <aimanakashah@gmail.com>
2024-02-01 02:15:44 +0800

    Second challenge to add line-through feature when element was clicked.

--- 16.0 Managing Component Tree Tutorial/src/components/List.jsx
+++ 16.0 Managing Component Tree Tutorial/src/components/List.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
+function crossNote(event) {
+  event.target.style.textDecoration = "line-through";
+  console.log(event);
+  //if I try method below, it will access multiple elements since every note is in the same class. thus cannot access the style of single element, (the note we want to line-through)
+  // document.getElementsByClassName("cross").style.textDecoration =
+  //   "line-through";
+}
+
 function List(props) {
-  return <li>{props.note}</li>;
+  return (
+    <li className="cross" onClick={crossNote}>
+      {props.note}
+    </li>
+  );
 }
 export default List;
